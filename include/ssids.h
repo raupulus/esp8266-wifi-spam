1c02df404c6720e9097c018a92196cc040346c38
Raúl Caro Pastorino <raul@fryntiz.dev>
2023-07-17 01:39:22 +0200

    Extract vars to files

---
+++ include/ssids.h
@@ -0,0 +1,78 @@
+
+const char ssids[] PROGMEM = {
+    "Lata de anchoas\n"
+    "Asociación de pinguinos\n"
+    "La luna es falsa\n"
+    "Cierra la puerta\n"
+    "Inter Piscinas\n"
+    "Abraham Linksys\n"
+    "Benjamin FrankLAN\n"
+    "Martin Router King\n"
+    "John Wilkes Bluetooth\n"
+    "Mi Wi-Fi esta colgado\n"
+    "Bill Wi the Science Fi\n"
+    "Lentejas con arroz\n"
+    "Flipando\n"
+    "LAN Solo\n"
+    "Hasky er man\n"
+    "El silencio de las LANs\n"
+    "House LANister\n"
+    "Mi perro se ha comido el wifi\n"
+    "Don Ping Pong\n"
+    "El Wi-Fi en el Norte\n"
+    "TON TON Soy el invierno\n"
+    "La promesa del LAN\n"
+    "Area 51\n"
+    "Seat Panda Wi-Fi\n"
+    "Planet Express\n"
+    "Wu Tang LAN\n"
+    "Androides Invasores\n"
+    "Esconde tu Wi-Fi\n"
+    "Cargando…\n"
+    "Buscando...\n"
+    "El Gazpacho se enfria\n"
+    "Text ###-#### for Password\n"
+    "Yell ____ for Password\n"
+    "El password es 1234\n"
+    "Este Wi-Fi no es Publico\n"
+    "Dora la Internet Explorer\n"
+    "404 Wi-Fi no localizado\n"
+    "Porque-Fi\n"
+    "Titanic Sound\n"
+    "Probando mi Wirfi\n"
+    "Cierra la puerta al salir\n"
+    "Cagando…\n"
+    "Boin 707 \n"
+    "Puesto de Churros\n"
+    "Eres mas viejo que internet\n"};
+
+/*
+
+const String ssids[5] = {
+    "Mi SSID 1",
+    "Mi SSID 2",
+    "Mi SSID 3",
+    "Mi SSID 4",
+    "Mi SSID 5"};
+
+const char ssids[] PROGMEM = {
+    "Mi SSID 1\n"
+    "Mi SSID 2\n"
+    "Mi SSID 3\n"
+    "Mi SSID 4\n"
+    "Mi SSID 5\n"};
+
+const char string_1[] PROGMEM = "Mi SSID 1";
+const char string_2[] PROGMEM = "Mi SSID 2";
+const char string_3[] PROGMEM = "Mi SSID 3";
+const char string_4[] PROGMEM = "Mi SSID 4";
+const char string_5[] PROGMEM = "Mi SSID 5";
+
+const char *const ssids[] PROGMEM = {
+    string_1,
+    string_2,
+    string_3,
+    string_4,
+    string_5};
+
+*/
---
+++ include/vars.h
@@ -0,0 +1,9 @@
+char emptySSID[32];
+uint8_t channelIndex = 0;
+uint8_t macAddr[6];
+uint8_t wifi_channel = 1;
+uint32_t currentTime = 0;
+uint32_t packetSize = 0;
+uint32_t packetCounter = 0;
+uint32_t attackTime = 0;
+uint32_t packetRateTime = 0;
